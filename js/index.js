4f7283f29f0db473b3735b3080f514535465cb31
nadyac <nadyacpena@gmail.com>
2017-01-19 08:49:20 -0500

    basic angular app that I will use for a Jasmine testing demo

---
+++ js/index.js
@@ -0,0 +1,46 @@
+var app = angular.module("myShoppingList", []); 
+app.controller("myCtrl", function($scope) {
+    $scope.products = ["Milk", "Bread", "Cheese"];
+    $scope.addItem = function () {
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}        
+        if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+        } else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
+    }
+    $scope.removeItem = function (x) {
+        $scope.errortext = "";    
+        $scope.products.splice(x, 1);
+    }
+    
+  $scope.rows = ['Paul','John','Lucie'];
+  $scope.temp = false;
+  
+  $scope.addRow = function(){
+    $scope.temp = false;
+    $scope.addName="";
+  };
+  
+  $scope.deleteRow = function(row){
+    $scope.rows.splice($scope.rows.indexOf(row),1);
+  };
+  
+  $scope.plural = function (tab){
+    return tab.length > 1 ? 's': ''; 
+  };
+  
+  $scope.addTemp = function(){
+    if($scope.temp) $scope.rows.pop(); 
+    else if($scope.addName) $scope.temp = true;
+    
+    if($scope.addName) $scope.rows.push($scope.addName);
+    else $scope.temp = false;
+  };
+  
+  $scope.isTemp = function(i){
+    return i==$scope.rows.length-1 && $scope.temp;
+  };
+  
+});
